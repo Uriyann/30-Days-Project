12696481f2a8df77b07054ae5ce93f6ad03af80e
Uriyann <joshuabarotea06@gmail.com>
2025-12-20 20:48:19 +0800

    unfinished js

---
+++ Clock/script.js
@@ -0,0 +1,34 @@
+// Clock Hands
+const secondHand = document.querySelector('.hand-second');
+const minuteHand = document.querySelector('.hand-minute');
+const hourHand = document.querySelector('.hand-hour');
+
+// Boxes
+const dayTime = document.getElementById('.time-Day');
+const hourTime = document.getElementById('.time-Hour');
+const minuteTime = document.getElementById('.time-Minute');
+const secondTime = document.getElementById('.time-Second');
+
+function setDate(){
+    const time = new Date();
+
+    // Seconds
+    const seconds = time.getSeconds();
+    const degreeSeconds = ((seconds / 60) * 360) + 90;
+
+    // Minutes
+    const minutes = time.getMinutes();
+    const degreeMinutes = ((minutes / 60) * 360) + 90;
+
+    // Hours
+    const hours = time.getHours();
+    const degreeHours = ((hours / 60) * 360) + 90;
+
+    secondHand.style.transform = `rotate(${degreeSeconds}deg)`;
+    minuteHand.style.transform = `rotate(${degreeMinutes}deg)`;
+    hourHand.style.transform = `rotate(${degreeHours}deg)`;
+}
+
+
+
+setInterval(setDate, 1000);
