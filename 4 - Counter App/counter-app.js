3079da0904271527de54d5d8ed230520431a824e
Uriyann <joshuabarotea06@gmail.com>
2025-12-27 21:57:21 +0800

    new

---
+++ 4 - Counter App/counter-app.js
@@ -0,0 +1,15 @@
+// Type your code below
+let a = 9;
+let b = 2.6;
+let c = 11;
+let d = a % b;
+let e = a % c;
+let f = c % e;
+
+// Don't change the line below
+console.log(`a = ${a}`)
+console.log(`b = ${b}`)
+console.log(`c = ${c}`)
+console.log(`d = ${d}`)
+console.log(`e = ${e}`)
+console.log(`f = ${f}`)
