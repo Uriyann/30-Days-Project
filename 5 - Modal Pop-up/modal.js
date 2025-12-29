e73c2dfb37737308d438c7a9531990d8830f7d97
Uriyann <joshuabarotea06@gmail.com>
2025-12-29 23:14:23 +0800

    finished js

---
+++ 5 - Modal Pop-up/modal.js
@@ -0,0 +1,22 @@
+// Getting the ID
+const modalExit = document.getElementById('modalExit');
+const cancelBtn = document.getElementById('cancelBtn');
+const subBtn = document.getElementById('subBtn');
+const modalPopup = document.getElementById('modalPopup');
+
+// Button Click Function
+subBtn.onclick = () => {
+    modalPopup.style.display = "block";
+    modalPopup.style.opacity = 1;
+};
+
+modalExit.onclick = () => {
+    modalPopup.style.display = "none";
+    modalPopup.style.opacity = 0;
+}
+
+cancelBtn.onclick = () => {
+    modalPopup.style.display = "none";
+    modalPopup.style.opacity = 0;
+}
+
