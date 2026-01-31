25ee58b47459fed24a77477944aa85963780ec31
Uriyann <joshuabarotea06@gmail.com>
2026-01-31 18:38:40 +0800

    finishing js

--- 10 - Calculator/calc.js
+++ 10 - Calculator/calc.js
@@ -15,3 +15,26 @@ button.addEventListener("click", (event) => {
   const value = event.target.textContent;
   console.log(value);
 });
+
+function handleDecimal(num) {
+  if (num === "." && currentInput === ".") {
+    currentInput += num;
+  }
+}
+
+function handleOperator(ope) {
+  if (currentInput === "") return;
+
+  previousInput = currentInput;
+  currentInput = "";
+  operator = ope;
+}
+
+function calculator() {
+  if (!previousInput || !currentInput || !operator) return;
+
+  const val1 = Number(currentInput);
+  const val2 = Number(previousInput);
+
+  let result;
+}
